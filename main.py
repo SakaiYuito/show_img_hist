17125253390708bcd360c2f53c2265f819365425
Yuito Sakai <cnj14061@nitech.ac.jp>
2024-06-20 10:52:20 +0900

    change caption agein

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def get_image():
 
     st.image(
         im,
-        caption='image from wikimedia commons, now at 2024/06/20 3',
+        caption='image from wikimedia commons, now at 2024/06/20 agein',
         use_column_width=True
     )
 
